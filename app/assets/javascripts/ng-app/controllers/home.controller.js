83c659c61a759ab9f3fd045e2c35bdc73b5be8be
stevula <brodericksteven@gmail.com>
2016-06-09 19:57:42 -0700

    Move posts collection to new top-level MainCtrl

--- app/assets/javascripts/ng-app/controllers/home.controller.js
+++ app/assets/javascripts/ng-app/controllers/home.controller.js
@@ -1,11 +1,5 @@
 angular.module('diggit')
 .controller('HomeCtrl', ['$scope', 'post', '$http', function($scope, post, $http) {
-    // TODO: this shouldn't need to refresh everytime
-    post.getPosts().then(function(resp) {
-        $scope.posts = resp.data;
-        post.posts = resp.data;
-    });
-
     $scope.incrementUpvotes = post.incrementUpvotes;
     $scope.decrementUpvotes = post.decrementUpvotes;
 }]);
--- app/assets/javascripts/ng-app/controllers/post.controller.js
+++ app/assets/javascripts/ng-app/controllers/post.controller.js
@@ -2,7 +2,7 @@ angular.module('diggit')
 .controller('PostCtrl', ['$scope', '$routeParams', 'post', '$filter',
     function($scope, $routeParams, post, $filter) {
         // only works if post.posts has already been populated (i.e. linked from index)
-        $scope.post = $filter('filter')(post.posts, {id: Number($routeParams.id)})[0];
+        $scope.post = $filter('filter')($scope.posts, {id: Number($routeParams.id)})[0];
 
         // TODO: DRYer solution? comment voting methods below identical to post 
         // voting methods
