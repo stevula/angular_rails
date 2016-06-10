62b3df19b09619f2721bd578860ce5782bc95d36
stevula <brodericksteven@gmail.com>
2016-06-09 20:42:08 -0700

    Fix bug where post show page would be blank if visited before index

--- app/assets/javascripts/ng-app/controllers/post.controller.js
+++ app/assets/javascripts/ng-app/controllers/post.controller.js
@@ -1,8 +1,12 @@
 angular.module('diggit')
 .controller('PostCtrl', ['$scope', '$routeParams', 'post', '$filter',
     function($scope, $routeParams, post, $filter) {
-        $scope.post = $filter('postFilter')(post.posts, {id: Number($routeParams.id)})[0];
+        $scope.postFactory = post;
 
+        // to ensure post view page updates if the service hasn't loaded posts yet
+        $scope.$watch('postFactory.posts', function(data) {
+            $scope.post = $filter('filter')(post.posts, {id: Number($routeParams.id)})[0];
+        });
         // TODO: DRYer solution? comment voting methods below identical to post 
         // voting methods
         $scope.incrementUpvotes = post.incrementUpvotes;
