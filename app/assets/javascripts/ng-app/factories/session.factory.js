bb1aa2d641d39213583e450b449d7d1e4b91893b
stevula <brodericksteven@gmail.com>
2016-06-14 10:19:22 -0700

    Add email field to login (for now).

--- app/assets/javascripts/ng-app/factories/session.factory.js
+++ app/assets/javascripts/ng-app/factories/session.factory.js
@@ -1,27 +1,7 @@
+// TODO: may not be needed anymore
 angular.module('diggit')
 .factory('session', ['Auth', function(Auth) {
-    
     var session = {};
-
-    // var credentials = {
-    //     username: 'admin',
-    //     email: 'admin@diggit.com',
-    //     password: 'password'
-    // };
-    // var login_config = {
-    //     headers: {
-    //         'X-HTTP-Method-Override': 'POST'
-    //     }
-    // };
-    // var logout_config = {
-    //     headers: {
-    //         'X-HTTP-Method-Override': 'DELETE'
-    //     }
-    // };
-
-    // // TODO: BUG: these are getting called during config phase
-    // session.login = Auth.login(credentials, login_config);
-    // session.logout = Auth.logout(logout_config);
 
     return session;
 }]);
